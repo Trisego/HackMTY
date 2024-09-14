6accc41c459cfbc9e6d3b4079407894e529b4156
HANS27barron <a01286990@tec.mx>
2024-09-14 13:54:37 -0600

    add model

--- Inicio.py
+++ Inicio.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("mosaicml/mpt-7b-storywriter", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("mosaicml/mpt-7b-storywriter", trust_remote_code=True)
 
 st.write("ghjkl")
 st.write("ghjk")
